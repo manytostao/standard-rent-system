cc8cafc3f8901530d68ed9fb17194676892fce04
Osmany Torres Leyva <manytostao@gmail.com>
2019-09-16 08:37:58 -0400

    Fix Manager module tests

--- server/src/manager/manager.controller.spec.ts
+++ server/src/manager/manager.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Manager } from './manager.entity';
+import { ManagerService } from './manager.service';
 import { ManagerController } from './manager.controller';
 
 describe('Manager Controller', () => {
-  let controller: ManagerController;
+    let controller: ManagerController;
+    let managerService: ManagerService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ManagerController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [ManagerController],
+            providers: [
+                ManagerService,
+                {
+                    provide: getRepositoryToken(Manager),
+                    useClass: Repository,
+                },
+            ],
+        }).compile();
 
-    controller = module.get<ManagerController>(ManagerController);
-  });
+        controller = module.get<ManagerController>(ManagerController);
+        managerService = module.get<ManagerService>(ManagerService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
--- server/src/manager/manager.service.spec.ts
+++ server/src/manager/manager.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Manager } from './manager.entity';
 import { ManagerService } from './manager.service';
 
 describe('ManagerService', () => {
-  let service: ManagerService;
+    let service: ManagerService;
+    let repository: Repository<Manager>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ManagerService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [ManagerService,
+                {
+                    provide: getRepositoryToken(Manager),
+                    useClass: Repository,
+                },
+            ],
+        }).compile();
 
-    service = module.get<ManagerService>(ManagerService);
-  });
+        service = module.get<ManagerService>(ManagerService);
+        repository = module.get<Repository<Manager>>(getRepositoryToken(Manager));
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('should call findOne on repository and return a promise with a House', async () => {
+        const manager = new Manager();
+        jest.spyOn(repository, 'findOne').mockImplementation(() => Promise.resolve(manager));
+        expect(service.findById('1')).toEqual(Promise.resolve(manager));
+        expect(repository.findOne).toBeCalledWith({where: {id: '1'}, relations: ['houses']});
+    });
+
+    it('should create a new manager using the repository', async () => {
+        const manager = new Manager();
+        jest.spyOn(repository, 'save').mockImplementation(() => Promise.resolve(manager));
+        expect(service.create(manager)).toEqual(Promise.resolve(manager));
+        expect(repository.save).toBeCalledWith(manager);
+    });
 });
